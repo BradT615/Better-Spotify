471e6440d4909034c61bafd19cf2a69c599f25a3
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-07-24 00:28:13 -0400

    fixed loggedin nav

--- public/main.js
+++ public/main.js
@@ -11,12 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function updateProfile(data) {
-    let imageUrl = data.images.length > 0 ? data.images[0].url : 'default-image.jpg';
+    let imageUrl = data.images.length > 0 ? data.images[0].url : 'assets/default-image.png';
 
     document.getElementById('user-name').textContent = data.display_name;
     document.getElementById('user-image').src = imageUrl;
-    document.getElementById('user-email').textContent = data.email;
-    document.getElementById('user-country').textContent = data.country;
   }
 
   function updateOAuth(data) {
