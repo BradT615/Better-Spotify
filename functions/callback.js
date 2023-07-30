6b529545c0c7fa851f05ad7154d8fbacc8eca332
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-07-30 03:43:37 -0400

    update uri

--- functions/callback.js
+++ functions/callback.js
@@ -51,7 +51,7 @@ exports.handler = async (event, context) => {
                 console.error('Supabase insert error:', supabaseErr);
               });
 
-            // let uri = process.env.FRONTEND_URI || 'https://bradt615spotify.netlify.app'
+            let uri = process.env.FRONTEND_URI || 'https://bradt615spotify.netlify.app'
 
             resolve({
               statusCode: 302,
