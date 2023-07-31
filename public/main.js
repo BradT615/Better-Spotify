660e84fd3fd79b1004bafcdd7c19f13d811c8a2f
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-07-31 17:33:48 -0400

    cookie test

--- public/main.js
+++ public/main.js
@@ -13,39 +13,47 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.user-image').forEach(img => img.src = imageUrl);
   }
 
-  var session_id = getCookie('session_id');
-  console.log('session_id in main.js:', session_id);
+  function checkSessionId() {
+    var session_id = getCookie('session_id');
+    console.log('session_id in main.js:', session_id);
 
-  if (session_id) {
-    // Use AJAX to call the serverless function, passing the session_id as a parameter
-    $.ajax({
-      url: '/.netlify/functions/get_user_profile',
-      xhrFields: {
-        withCredentials: true
-      },
-      success: function(response) {
-        const data = JSON.parse(response); // Parse the JSON string into an object
-        updateProfile(data);
+    if (session_id) {
+      // Use AJAX to call the serverless function, passing the session_id as a parameter
+      $.ajax({
+        url: '/.netlify/functions/get_user_profile',
+        xhrFields: {
+          withCredentials: true
+        },
+        success: function(response) {
+          const data = JSON.parse(response); // Parse the JSON string into an object
+          updateProfile(data);
+        
+          $('#login').hide();
+          $('#loggedin').show();
       
-        $('#login').hide();
-        $('#loggedin').show();
-    
-        // Log the session_id here
-        var session_id_after_ajax = getCookie('session_id');
-        console.log('session_id after AJAX:', session_id_after_ajax);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.error('AJAX error:', textStatus, ', Details:', errorThrown);
-        console.error('Response:', jqXHR.responseText);
-      }
-    });     
-  } else {
-    // render initial screen
-    $('#login').show();
-    $('#loggedin').hide();
-
-    document.getElementById('login-button').addEventListener('click', function() {
-      window.location = '/.netlify/functions/login';
-    }, false);
+          // Log the session_id here
+          var session_id_after_ajax = getCookie('session_id');
+          console.log('session_id after AJAX:', session_id_after_ajax);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.error('AJAX error:', textStatus, ', Details:', errorThrown);
+          console.error('Response:', jqXHR.responseText);
+        }
+      });     
+    } else {
+      // render initial screen
+      $('#login').show();
+      $('#loggedin').hide();
+    }
   }
+
+  // Check session_id when page is loaded
+  checkSessionId();
+
+  // Also check session_id when login button is clicked
+  document.getElementById('login-button').addEventListener('click', function() {
+    window.location = '/.netlify/functions/login';
+    // Check session_id again after a delay to give the login function time to complete
+    setTimeout(checkSessionId, 2000);  // Adjust delay as needed
+  }, false);
 });
