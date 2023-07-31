27c0c21448b4f897a88ea7de40e549936984f575
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-07-31 14:31:35 -0400

    debug logs

--- public/main.js
+++ public/main.js
@@ -18,17 +18,20 @@ document.addEventListener("DOMContentLoaded", function() {
   if (session_id) {
     // Use AJAX to call the serverless function, passing the session_id as a parameter
     $.ajax({
-        url: '/.netlify/functions/get_user_profile',
-        headers: {
-          'Cookie': `session_id=${session_id}`
-        },
-        success: function(response) {
-          const data = JSON.parse(response); // Parse the JSON string into an object
-          updateProfile(data);
-
-          $('#login').hide();
-          $('#loggedin').show();
-        }
+      url: '/.netlify/functions/get_user_profile',
+      headers: {
+        'Cookie': `session_id=${session_id}`
+      },
+      success: function(response) {
+        const data = JSON.parse(response);
+        updateProfile(data);
+    
+        $('#login').hide();
+        $('#loggedin').show();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error('AJAX error:', textStatus, errorThrown);
+      }
     });
   } else {
     // render initial screen
