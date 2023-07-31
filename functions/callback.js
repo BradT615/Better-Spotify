47376d2ef70c1cddf6a1a7a1957aaf37bec53ad9
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-07-31 16:08:57 -0400

    removed httpOnly

--- functions/callback.js
+++ functions/callback.js
@@ -50,7 +50,7 @@ exports.handler = async (event, context) => {
       statusCode: 302,
       headers: {
         Location: uri,
-        'Set-Cookie': `session_id=${session_id}; Secure; HttpOnly; SameSite=Lax`
+        'Set-Cookie': `session_id=${session_id}; Secure; SameSite=Lax`
       },
       body: ''
     };
