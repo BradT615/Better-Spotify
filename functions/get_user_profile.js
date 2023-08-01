a53704dda4a25cd7aa2e11100cfb775c36eeadb9
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-07-31 23:55:07 -0400

    cleaned

--- functions/get_user_profile.js
+++ functions/get_user_profile.js
@@ -6,7 +6,6 @@ exports.handler = async (event, context) => {
   // Parse the session_id from the cookies in the incoming request
   let cookies = cookie.parse(event.headers.cookie || '');
   let session_id = cookies.session_id;
-  console.log('session_id in get_user_profile.js:', session_id);
 
   // Get the user's access token from the database
   const { data: user, error } = await supabase
@@ -15,54 +14,38 @@ exports.handler = async (event, context) => {
     .eq('id', session_id)
     .single();
 
+  // If error occurs or no user is found, return appropriate error response
   if (error || !user) {
-    console.error('Failed to retrieve user:', error);
     return {
-      statusCode: 200,
+      statusCode: 401,
       headers: {
         'Access-Control-Allow-Origin': 'https://bradt615spotify.netlify.app',
         'Access-Control-Allow-Credentials': 'true'
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify({ error: 'Authentication error.' })
     };
   }
 
   let access_token = user.access_token;
-
   let requestOptions = {
     url: 'https://api.spotify.com/v1/me',
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
+    headers: { 'Authorization': 'Bearer ' + access_token },
     json: true
   };
 
   return new Promise((resolve, reject) => {
-    request.get(requestOptions, function(error, response, body) {
+    request.get(requestOptions, async function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        resolve({
-          statusCode: 200,
-          body: JSON.stringify(body)
-        });
+        resolve({ statusCode: 200, body: JSON.stringify(body) });
       } else {
-        // Check if the error is due to revoked access
         if (response && response.statusCode === 401) {
           // Delete the user's record from the database
-          supabase
+          await supabase
             .from('users')
             .delete()
-            .eq('id', session_id)
-            .then(supabaseRes => {
-              console.log('Supabase delete response:', supabaseRes);
-            })
-            .catch(supabaseErr => {
-              console.error('Supabase delete error:', supabaseErr);
-            });
+            .eq('id', session_id);
         }
-        reject({
-          statusCode: 500,
-          body: JSON.stringify({ message: 'Internal Server Error: Failed to retrieve user profile.' })
-        });
+        reject({ statusCode: 500, body: JSON.stringify({ message: 'Internal Server Error: Failed to retrieve user profile.' }) });
       }
     });
   });
--- public/main.js
+++ public/main.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  }
 
   function updateProfile(data) {
     let imageUrl = data.images.length > 0 ? data.images[0].url : 'assets/default-image.png';
@@ -13,45 +7,39 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.user-image').forEach(img => img.src = imageUrl);
   }
 
-  function checkSessionId() {
-    // No need to get session_id from cookie here because it's HTTP-only
-  
-    // Use AJAX to call the serverless function
+  function updateUserState(isLoggedIn, data = null) {
+    if (isLoggedIn) {
+      updateProfile(data);
+      $('#login').hide();
+      $('#loggedin').show();
+    } else {
+      $('#login').show();
+      $('#loggedin').hide();
+    }
+  }
+
+  function checkUserSession() {
     $.ajax({
       url: '/.netlify/functions/get_user_profile',
       xhrFields: {
         withCredentials: true
       },
       success: function(response) {
-        const data = JSON.parse(response); // Parse the JSON string into an object
-        updateProfile(data);
-      
-        $('#login').hide();
-        $('#loggedin').show();
-        
+        const data = JSON.parse(response);
+        updateUserState(true, data);
       },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // If the AJAX call fails, it means the user is not logged in
-        console.error('AJAX error:', textStatus, ', Details:', errorThrown);
-        console.error('Response:', jqXHR.responseText);
-        console.error('Error object:', jqXHR);
-
-        // delete any possible cookies
-        document.cookie = "session_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        
-        $('#login').show();
-        $('#loggedin').hide();
+      error: function() {
+        updateUserState(false);
       }
     });
   }
 
-  // Check session_id when page is loaded
-  checkSessionId();
+  // Check user session when page is loaded
+  checkUserSession();
 
-  // Also check session_id when login button is clicked
+  // Also check user session when login button is clicked
   document.getElementById('login-button').addEventListener('click', function() {
     window.location = '/.netlify/functions/login';
-    // Check session_id again after a delay to give the login function time to complete
-    setTimeout(checkSessionId, 2000);  // Adjust delay as needed
+    setTimeout(checkUserSession, 2000);
   }, false);
 });
