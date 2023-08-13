1c6d6a562ff799a6a93f08cfe59b81b31cf9b56d
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-08-13 17:40:17 -0400

    added logging

--- functions/get_access_token.js
+++ functions/get_access_token.js
@@ -5,12 +5,19 @@ exports.handler = async (event, context) => {
   let cookies = cookie.parse(event.headers.cookie || '');
   let session_id = cookies.session_id;
 
+  console.log("Parsed cookies:", cookies); // Log the parsed cookies
+  console.log("Session ID:", session_id);   // Log the extracted session_id
+
   // Get the user's access token from the database
   const { data: user, error } = await supabase
     .from('users')
     .select('access_token')
     .eq('id', session_id)
     .single();
+
+  if (user) {
+    console.log("User retrieved:", user); // Log the retrieved user object
+  }
 
   if (error || !user) {
     console.error('Failed to retrieve user:', JSON.stringify(error));
@@ -25,6 +32,8 @@ exports.handler = async (event, context) => {
   }
 
   let access_token = user.access_token;
+
+  console.log("Access Token:", access_token); // Log the extracted access token
 
   return {
     statusCode: 200,
