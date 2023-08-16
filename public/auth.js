de57163ad1c0ef092955fce67401f205324ae7e0
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-08-16 19:17:47 -0400

    new codebase structure

---
+++ public/auth.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    function checkUserSession(retryCount = 0) {
+        $.ajax({
+            url: '/.netlify/functions/get_user_profile',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(response) {
+                loadDeviceSpecificScript();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                if ((jqXHR.status === 401 || jqXHR.status === 502) && retryCount < 1) {
+                    refreshTokenAndRetry();
+                } else {
+                    console.error("Error fetching user profile:", errorThrown);
+                    deleteUser();
+                }
+            }
+        });
+    }
+
+    function refreshTokenAndRetry(retryCount = 1) {
+        if (retryCount > 3) {
+            deleteUser();
+            return;  
+        }
+
+        $.ajax({
+            url: '/.netlify/functions/refresh_token',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(response) {
+                checkUserSession();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status === 401 || jqXHR.status === 502) {
+                    console.error("User possibly revoked access or there was an error refreshing the token.");
+                    deleteUser();
+                } else {
+                    refreshTokenAndRetry(retryCount + 1);
+                }
+            }
+        });
+    }
+
+    function deleteUser() {
+        $.ajax({
+            url: '/.netlify/functions/delete_user',
+            type: 'DELETE',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(response) {
+                console.log("User data deleted successfully.");
+                // Optionally, redirect the user to a login page or display a message
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error("Failed to delete user data:", errorThrown);
+                // Optionally, display an error message
+            }
+        });
+    }
+
+    checkUserSession();
+
+});
+
+function loadDeviceSpecificScript() {
+    let scriptFile = window.innerWidth <= 640 ? "mobile.js" : "desktop.js";
+    let script = document.createElement('script');
+    script.src = scriptFile;
+    document.head.appendChild(script);
+}
